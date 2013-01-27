fe9679484ac8fbe3e155a11d0b6390c40008783b
msulliv <medfoundry@gmail.com>
2013-01-27 04:07:58 -0800

    Got topic detail pages displaying.

---
+++ ajax/topic91.js
@@ -0,0 +1,71 @@
+{
+    "Result"
+:
+    {
+        "Error"
+    :
+        "False", "Total"
+    :
+        "1", "Query"
+    :
+        null, "Language"
+    :
+        "English", "Topics"
+    :
+        {
+            "Id"
+        :
+            "91", "Title"
+        :
+            "Make the Most of Your Child&rsquo;s Visit to the Doctor (Ages&nbsp;5&nbsp;to&nbsp;10)", "Categories"
+        :
+            "Doctor Visits, Children and Teens", "MyHFTitle"
+        :
+            null, "MyHFDescription"
+        :
+            null, "MyHFCategory"
+        :
+            null, "LastUpdate"
+        :
+            "12/12/2012 5:51:27 PM", "ImageUrl"
+        :
+            "http://healthfinder.gov/HealthTopics/glmedia/icons/makethemostofyourchildsvisittothedoctor-ages5to10.jpg", "ImageAlt"
+        :
+            "Smiling boy", "AccessibleVersion"
+        :
+            "http://healthfinder.gov/HealthTopics/Category/parenting/doctor-visits/make-the-most-of-your-childs-visit-to-the-doctor-ages-5-to-10", "RelatedItems"
+        :
+            [
+                {"Title":"Healthy Snacks: Quick tips for parents", "Url":"http://healthfinder.gov/HealthTopics/Category/nutrition-and-physical-activity/nutrition/healthy-snacks-quick-tips-for-parents", "Type":"tool"},
+                {"Title":"Prevent Bullying: Quick tips for parents", "Url":"http://healthfinder.gov/HealthTopics/Category/parenting/healthy-communication-and-relationships/prevent-bullying-quick-tips-for-parents", "Type":"tool"},
+                {"Title":"Swim Safely: Quick tips", "Url":"http://healthfinder.gov/HealthTopics/Category/parenting/nutrition-and-physical-activity/swim-safely-quick-tips", "Type":"tool"},
+                {"Title":"Get Your Child&rsquo;s Shots on Schedule", "Url":"http://healthfinder.gov/HealthTopics/Category/doctor-visits/shotsvaccines/get-your-childs-shots-on-schedule", "Type":"topic"},
+                {"Title":"Protect Your Child from Injury", "Url":"http://healthfinder.gov/HealthTopics/Category/everyday-healthy-living/safety/protect-your-child-from-injury", "Type":"topic"},
+                {"Title":"Help Your Child Stay at a Healthy Weight", "Url":"http://healthfinder.gov/HealthTopics/Category/parenting/nutrition-and-physical-activity/help-your-child-stay-at-a-healthy-weight", "Type":"topic"},
+                {"Title":"Take Care of Your Child&rsquo;s Teeth", "Url":"http://healthfinder.gov/HealthTopics/Category/parenting/doctor-visits/take-care-of-your-childs-teeth", "Type":"topic"},
+                {"Title":"Talk to Your Kids about Tobacco, Alcohol, and Drugs", "Url":"http://healthfinder.gov/HealthTopics/Category/parenting/healthy-communication-and-relationships/talk-to-your-kids-about-tobacco-alcohol-and-drugs", "Type":"topic"},
+                {"Title":"Get Your Child&rsquo;s Vision Checked", "Url":"http://healthfinder.gov/HealthTopics/Category/doctor-visits/screening-tests/get-your-childs-vision-checked", "Type":"topic"},
+                {"Title":"Ride Your Bike Safely", "Url":"http://healthfinder.gov/HealthTopics/Category/nutrition-and-physical-activity/physical-activity/ride-your-bike-safely", "Type":"topic"},
+                {"Title":"Prevent Mosquito and Tick&nbsp;Bites", "Url":"http://healthfinder.gov/HealthTopics/Category/parenting/safety/prevent-mosquito-and-tick-bites", "Type":"topic"},
+                {"Title":"Talk to Your Kids about Sex", "Url":"http://healthfinder.gov/HealthTopics/Category/parenting/healthy-communication-and-relationships/talk-to-your-kids-about-sex", "Type":"topic"},
+                {"Title":"Prevent Allergy and Asthma Attacks at Home", "Url":"http://healthfinder.gov/HealthTopics/Category/parenting/safety/prevent-allergy-and-asthma-attacks-at-home", "Type":"topic"}
+            ], "Sections"
+        :
+            [
+                {"Title":"The Basics", "Description":"Kids ages 5 to 10 need to go to the doctor or nurse for a &ldquo;well-child visit&rdquo; once a year.", "Content":"<p>Kids ages 5 to 10 need to go to the doctor or nurse for a &ldquo;well-child visit&rdquo; once a year. A well-child visit is when you take your child to the doctor for a full checkup, separate from any other visit for sickness or injury.</p><p>At these visits, the doctor or nurse can help catch problems early, when they may be easier to treat. Make the most of your child&rsquo;s visit by:</p><ul ><li>Gathering important information</li><li>Making a list of questions for the doctor</li><li>Knowing what to expect from the visit</li></ul><p><strong>What about cost?</strong><br />Well-child visits are covered under the new <a href=\"http://www.healthcare.gov/law/features/rights/preventive-care/index.html\"  >Affordable Care Act</a>. Depending on your insurance plan, your child may be able to get well-child checkups at no cost to you. Check with your insurance provider.</p><p><strong>How do I know if my child is growing and developing on schedule?</strong><br />Your child&rsquo;s doctor or nurse can help you identify the signs (called developmental milestones) to look for in your child. This is an important part of the well-child visit.</p><p>Some developmental milestones for children ages 5 to 10 include:</p><ul ><li>Developing skills for success in school (like sorting, counting, and language&nbsp;skills)</li><li>Taking over body care (like bathing, brushing teeth, and getting dressed)</li><li>Learning from mistakes or failures and trying again</li><li>Helping out with simple chores</li><li>Following family rules</li><li>Bringing friends home to play and getting invited to friends&rsquo; homes</li><li>Joining school clubs, teams, or other activities</li></ul><p><a href=\"http://www.brightfutures.org/tools/BFtoolsMC.pdf\"  >Learn more about the social and emotional development of kids ages 5 to 10 [PDF - 848 KB]</a>. </p>"},
+                {"Title":"Take Action!", "Description":"Keep a list of questions you want to ask the doctor. Take this list to the next appointment.", "Content":"<p>Take these steps to help you and your child get the most out of visits to the doctor.</p><p><strong>Gather important information.</strong><br />Take any medical records you have to the appointment, including a record of shots your child has received. Make a list of any important changes in your child&rsquo;s life since the last doctor&rsquo;s visit, like:</p><ul><li>A new brother or sister</li><li>A serious illness or death in the family</li><li>A new school or a move to a new neighborhood</li></ul><p><a href=\"https://familyhistory.hhs.gov/fhh-web/home.action\"  >Use this tool to keep track of your child&rsquo;s family health history</a>.</p><p><strong>Make a list of questions you want to ask the doctor.</strong><br />This visit is a great time to ask the doctor or nurse any questions related to:</p><ul ><li>A medical condition your child has (like asthma)</li><li>Changes in behavior or mood</li><li>Problems in school with learning or friends</li></ul><p>Some important questions include:</p><ul ><li>Is my child up-to-date on shots?</li><li>How can I make sure my child is getting enough physical activity?</li><li>Is my child at a healthy weight?</li></ul><p>Take a notepad and write down the answers so you remember them later.</p><p><strong>Know what to expect.</strong><br />There are 2 main parts to each well-child visit. The doctor or nurse will ask you questions about your child and do a physical exam. The doctor or nurse will also use this information to update your child&rsquo;s medical history.</p><p><strong><em>Questions the doctor or nurse may ask:</em></strong></p><ul ><li>Behavior &ndash; Does your child have trouble following directions at home or at&nbsp;school?</li><li>Health &ndash; Does your child often complain of headaches or other pain?</li><li>School &ndash; Does your child look forward to going to school?</li><li>Activity &ndash; What does your child like to do after school?</li><li>Eating habits &ndash; What does your child eat on a normal day?</li><li>Family &ndash; Have there been any changes in your family since your last visit?</li></ul><p>Your answers to these questions will help the doctor or nurse make sure your child is healthy. Find out more about questions the doctor may ask:</p><ul ><li><a href=\"http://brightfutures.aap.org/pdfs/Visit%20Forms%20by%20Age%20110/5,%206%20Year/CMCh.SQ.5,6yr.pdf\"  >Ages 5 and 6 [PDF - 246 KB]</a> </li><li><a href=\"http://brightfutures.aap.org/pdfs/Visit%20Forms%20by%20Age%20110/7,%208%20Year/C.MCh.SQ.7,8yr.pdf\"  >Ages 7 and 8 [PDF - 246 KB]</a> </li><li><a href=\"http://brightfutures.aap.org/pdfs/Visit%20Forms%20by%20Age%20110/9,%2010%20Year/C.MCh.SQ.9,10yr.pdf\"  >Ages 9 and 10 [PDF - 246 KB]</a> </li></ul><p><strong><em>The doctor or nurse will also check your child&rsquo;s body.</em></strong><br />The doctor or nurse will:</p><ul ><li>Measure your child&rsquo;s height and weight</li><li>Check your child&rsquo;s blood pressure</li><li><a href=\"http://healthfinder.gov/HealthTopics/ViewTopic.aspx?topicId=59\">Give needed shots</a></li><li><a href=\"http://healthfinder.gov/HealthTopics/ViewTopic.aspx?topicId=67\">Check your child&rsquo;s vision</a></li><li>Check your child&rsquo;s body parts</li></ul>"},
+                {"Title":"Start Today: Small Steps", "Description":null, "Content":"<ul><li><a href=\"http://www.brightfuturesforfamilies.org/pdf/ThinkingAheadMChild.PDF\">Check out these tips on parenting kids ages 5 to 10 [PDF - 367 KB]</a>. </li><li><a href=\"http://kidshealth.org/kid/htbw/htbw_main_page.html\">Share this Web site with your child and learn about the body together</a>. </li><li>Set a rule for how much TV your child can watch each day.</li></ul>"}
+            ], "Analytics"
+        :
+            "<script type='text/javascript'>var healthfinder_search_api_domain='http://healthfinder.gov';\nvar healthfinder_search_api_key='demo_analytic_key';\nvar healthfinder_search_api_q = '';\nvar healthfinder_search_api_lang = 'en';\nvar healthfinder_search_api_type = '';\nvar healthfinder_search_api_topics = '91';\nvar healthfinder_search_api_tools = '';\nvar healthfinder_search_api_categories = '';\nvar healthfinder_search_api_return_type = 'json';\n</script><script type='text/javascript' src='http://healthfinder.gov/Scripts/hf_search_api_analytics.js'></script>", "HealthfinderLogo"
+        :
+            "http://healthfinder.gov/HealthTopics/glmedia/bw_hf_logo.gif", "HealthfinderUrl"
+        :
+            "http://healthfinder.gov"
+        }
+    ,
+        "Analytics"
+    :
+        "<script type='text/javascript'>var healthfinder_search_api_domain='http://healthfinder.gov';\nvar healthfinder_search_api_key='demo_analytic_key';\nvar healthfinder_search_api_q = '';\nvar healthfinder_search_api_lang = 'en';\nvar healthfinder_search_api_type = '';\nvar healthfinder_search_api_topics = '91';\nvar healthfinder_search_api_tools = '';\nvar healthfinder_search_api_categories = '';\nvar healthfinder_search_api_return_type = 'json';\n</script><script type='text/javascript' src='http://healthfinder.gov/Scripts/hf_search_api_analytics.js'></script>"
+    }
+}
