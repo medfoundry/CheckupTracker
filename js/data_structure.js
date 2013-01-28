bf57a74400d20f183346ec4e763cbef3ecddc69a
msulliv <medfoundry@gmail.com>
2013-01-28 00:29:32 -0800

    Got json child data wired up. Kids still hard-coded.

---
+++ js/data_structure.js
@@ -0,0 +1,82 @@
+{
+    "children": [
+    {
+        "child": {
+            "child_name": "Declan",
+            "child_dob": "12/05/2006",
+            "physician": "Dr Prina",
+            "checkups": [
+                {
+                    "checkup": {
+                        "checkup_name": "6 Year Checkup",
+                        "visit_date": "12/05/2012",
+                        "visit_time": "10:15AM",
+                        "questions_text": "Is my child up-to-date on shots?\n \nHow can I make sure my child is getting enough physical activity?\n \nIs my child at a healthy weight?",
+                        "sched_complete": true,
+                        "plan_complete": true,
+                        "record_complete": true,
+                        "height": 60,
+                        "weight": 75,
+                        "systolic_blood_pressure": 120,
+                        "diastolic_blood_pressure": 80,
+                        "notes": "Again a long text field, which could be <b>formatted</b>.",
+                        "hc_provider": "Dr Prina",
+                        "vision_rt_eye": 20,
+                        "vision_left_eye": 30,
+                        "immunizations": [
+                            {
+                                "immunization": {
+                                    "Trade_Name": "InfluZa",
+                                    "vaccine": "flu",
+                                    "dose": 1,
+                                    "notes": "Lots of ice on this one for 24 hours."
+                                }
+                            }
+                        ],
+                        "hc_clinic": "Partners in Pediatrics"
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "child": {
+            "child_name": "Marin",
+            "child_dob": "12/05/2001",
+            "physician": "Dr Dean",
+            "checkups": [
+                {
+                    "checkup": {
+                        "checkup_name": "6 Year Checkup",
+                        "visit_date": "12/05/2012",
+                        "visit_time": "10:15AM",
+                        "questions_text": "Is my child up-to-date on shots?\n \nHow can I make sure my child is getting enough physical activity?\n \nIs my child at a healthy weight?",
+                        "sched_complete": true,
+                        "plan_complete": true,
+                        "record_complete": true,
+                        "height": 60,
+                        "weight": 75,
+                        "systolic_blood_pressure": 120,
+                        "diastolic_blood_pressure": 80,
+                        "notes": "Again a long text field, which could be <b>formatted</b>.",
+                        "hc_provider": "Dr Prina",
+                        "vision_rt_eye": 20,
+                        "vision_left_eye": 30,
+                        "immunizations": [
+                            {
+                                "immunization": {
+                                    "Trade_Name": "InfluZa",
+                                    "vaccine": "flu",
+                                    "dose": 1,
+                                    "notes": "Lots of ice on this one for 24 hours."
+                                }
+                            }
+                        ],
+                        "hc_clinic": "Partners in Pediatrics"
+                    }
+                }
+            ]
+        }
+    }
+]
+}
